8712371718393de5579c4b5bd5ef3d4d5bc2d3b0
ahmed hamed <ahmed.hd1011@gmail.com>
2018-09-01 19:19:25 +0200

    adding first section

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.test1', {
+	strings: ["Welcome To our Team"],
+	typeSpeed: 100
+	
+  });
+
+
+$("#wrapper").animate({width:'100%'},500)
+$("#wrapper").animate({height:'100%'},500,function(){
+    $(".typedT").fadeIn(700,function(){
+        $(".container" ).fadeIn(1000)
+    })
+})
